bac48a458b1004369fa319200bbb048a003e0b97
evantianx <evantianx@gmail.com>
2020-08-26 16:07:36 +0800

    feat: SSR with urql and nextjs

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,87 +1,12 @@
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
-import { createClient, Provider, fetchExchange, dedupExchange } from "urql";
-import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
+import { CSSReset, ThemeProvider } from "@chakra-ui/core";
 import theme from "../theme";
-import {
-  MeDocument,
-  LoginMutation,
-  MeQuery,
-  RegisterMutation,
-  LogoutMutation,
-} from "../generated/graphql";
-
-function betterUpdateQuery<Result, Query>(
-  cache: Cache,
-  qi: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
-}
-
-const client = createClient({
-  url: "http://localhost:4000/graphql",
-  fetchOptions: { credentials: "include" },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      updates: {
-        Mutation: {
-          logout: (_result, _args, cache, _info) => {
-            betterUpdateQuery<LogoutMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => ({ me: null })
-            );
-          },
-          login: (_result, _args, cache, _info) => {
-            betterUpdateQuery<LoginMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.login.errors) {
-                  return query;
-                } else {
-                  return {
-                    me: result.login.user,
-                  };
-                }
-              }
-            );
-          },
-          register: (_result, _args, cache, _info) => {
-            betterUpdateQuery<RegisterMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.register.errors) {
-                  return query;
-                } else {
-                  return {
-                    me: result.register.user,
-                  };
-                }
-              }
-            );
-          },
-        },
-      },
-    }),
-    fetchExchange,
-  ],
-});
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
-      <ThemeProvider theme={theme}>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,22 @@
 import { NavBar } from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>Hello world</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostQuery();
+  return (
+    <>
+      <NavBar />
+      <div>Hello world</div>
+      <br />
+      {!data ? (
+        <div>loading</div>
+      ) : (
+        data.posts.map((p) => <h3 key={p.id}>{p.title}</h3>)
+      )}
+    </>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
